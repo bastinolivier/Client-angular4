72df4aa65ca7e45a5fffd67d969c9d493a4c271e
Ryu <annabimountassar@gmail.com>
2018-04-16 16:08:52 +0200

    squelette applicatif Angular 4 , fakebackend API register/login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import {fakeBackendProvider, JwtInterceptor} from "./_helpers";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AlertService, AuthenticationService, UserService} from "./_services";
+import {AuthGuard} from "./_guards";
+import {AlertComponent} from "./_directives";
+import {routing} from "./app.routing";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    AlertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule,
+    routing,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
